9113c718e1d1fb2588c4922e09deb92eef52a43e
bolamboaz <bolamboaz@gmail.com>
2021-09-11 14:46:56 +0800

    routers add

--- routers/posts.js
+++ routers/posts.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../models/Posts');
 
+router.get('/', () => {
+    res.send("hello wold")
+});
+
 router.post('/', (req,res) => {
     const post = new Post({
         title: req.body.title,
