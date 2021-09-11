0415f88d58181b01bf3b75227b9f7330adbf5192
bolamboaz <bolamboaz@gmail.com>
2021-09-11 14:33:30 +0800

    add post route and mongodb

--- index.js
+++ index.js
@@ -2,7 +2,23 @@ const express = require("express");
 const app = express();
 const importData = require("./data.json")
 const importTravelData = require("./travel-vlog.json")
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const postRoute = require('./routers/posts');
+
+
+require('dotenv/config');
 let port = process.env.PORT || 3000;
+
+
+app.use(bodyParser.json)
+
+mongoose.connect(
+    process.env.DB_CONNECTION,
+    () => {
+    console.log('connect db')
+})
 
 app.get("/", (req,res) => {
     res.send("hello world")
@@ -16,6 +32,10 @@ app.get("/travel-vlog", (req, res) => {
     res.send(importTravelData)
 })
 
+//ROUTES
+app.use('/posts', postRoute)
+
+
 app.listen(port, () => {
-    console.log(`exampl;e app is listening on port ${port}`)
+    console.log(`example app is listening on port ${port}`)
 })
---
+++ routers/posts.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const Post = require('../models/Posts');
+
+router.post('/', (req,res) => {
+    const post = new Post({
+        title: req.body.title,
+        description: req.body.description
+    })
+    
+    post.save()
+    .then( data => {
+        res.json(data)
+    })
+    .catch(err => {
+        res.json({message: err})
+    })
+})
+
+module.exports = router;
