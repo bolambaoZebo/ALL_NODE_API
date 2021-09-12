8d4051ddbc43b9b103e79eaa2a842f779c724dd5
bolamboaz <bolamboaz@gmail.com>
2021-09-12 12:06:35 +0800

    clean index

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-const importData = require("./data.json")
 
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
