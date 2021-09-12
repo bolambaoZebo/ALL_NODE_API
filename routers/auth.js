2043c5270eadd7c7ae956338892f95b8301b31ec
bolamboaz <bolamboaz@gmail.com>
2021-09-12 17:14:03 +0800

    auth

--- routers/auth.js
+++ routers/auth.js
@@ -1,92 +1,92 @@
-const express = require('express');
-const router = express.Router();
-const User = require('../models/User');
+// const express = require('express');
+// const router = express.Router();
+// const User = require('../models/User');
 
-const Joi = require('@hapi/joi');
+// const Joi = require('@hapi/joi');
 
-const schema = {
-    name: Joi.string().min(6).required(),
-    password: Joi.string().min(6).required(),
-    location: Joi.string().min(6).required()
-    // email: Joi.string().min(6).required().email(),
-}
+// const schema = {
+//     name: Joi.string().min(6).required(),
+//     password: Joi.string().min(6).required(),
+//     location: Joi.string().min(6).required()
+//     // email: Joi.string().min(6).required().email(),
+// }
 
-router.get('/', async (req, res) => {
-    // res.send("hello wold")
-    try{
-        const users = await User.find();
-        res.json(users);
-       }catch(err){
-            res.json({message: err})
-        }
-});
-
-router.post('/register', async (req,res) => {
-
-    //VALIDATE DATA FROM THE USER
-    // const validation = Joi.validate(req.body, schema)
-
-    // res.send(validation)
-//     const user = new User({
-//         name: req.body.name,
-//         password: req.body.password,
-//         phone: req.body.phone,
-//         location: req.body.location,
-//         email: req.body.email,
-//     })
-    
-//    try{
-//     const saveUser = await user.save()
-//     res.json(saveUser)
-//    }catch(err){
-//         res.status(404).send(err)
-//     }
-})
-
-// router.post('/login', async (req,res) => {
-
-//     const post = new Post({
-//         title: req.body.title,
-//         description: req.body.description
-//     })
-    
-//    try{
-//     const savePost = await post.save()
-//     res.json(savePost)
-//    }catch(err){
-//         res.json({message: err})
-//     }
-// })
-
-// router.get('/:postId', async (req,res) => {
+// router.get('/', async (req, res) => {
+//     // res.send("hello wold")
 //     try{
-//         const post = await Post.findById(req.params.postId)
-//         res.json(post)
-//     }catch (err){
-//         res.json({ message: err })
-//     }
+//         const users = await User.find();
+//         res.json(users);
+//        }catch(err){
+//             res.json({message: err})
+//         }
+// });
+
+// router.post('/register', async (req,res) => {
+
+//     //VALIDATE DATA FROM THE USER
+//     // const validation = Joi.validate(req.body, schema)
+
+//     // res.send(validation)
+// //     const user = new User({
+// //         name: req.body.name,
+// //         password: req.body.password,
+// //         phone: req.body.phone,
+// //         location: req.body.location,
+// //         email: req.body.email,
+// //     })
+    
+// //    try{
+// //     const saveUser = await user.save()
+// //     res.json(saveUser)
+// //    }catch(err){
+// //         res.status(404).send(err)
+// //     }
 // })
 
-// router.delete('/:postId', async (req,res) => {
-//     try{
-//         const removePost = await Post.remove({_id: req.params.postId})
-//         res.json(removePost)
-//     }catch (err){
-//         res.json({ message: err })
-//     }
-// })
+// // router.post('/login', async (req,res) => {
 
-// router.patch('/:postId', async (req,res) => {
-//     try{ 
-//         const updatePost = await Post.updateOne(
-//             { _id: req.params.postId },
-//             { $set: {title: req.body.title }})
+// //     const post = new Post({
+// //         title: req.body.title,
+// //         description: req.body.description
+// //     })
+    
+// //    try{
+// //     const savePost = await post.save()
+// //     res.json(savePost)
+// //    }catch(err){
+// //         res.json({message: err})
+// //     }
+// // })
 
-//         res.json(updatePost)
+// // router.get('/:postId', async (req,res) => {
+// //     try{
+// //         const post = await Post.findById(req.params.postId)
+// //         res.json(post)
+// //     }catch (err){
+// //         res.json({ message: err })
+// //     }
+// // })
+
+// // router.delete('/:postId', async (req,res) => {
+// //     try{
+// //         const removePost = await Post.remove({_id: req.params.postId})
+// //         res.json(removePost)
+// //     }catch (err){
+// //         res.json({ message: err })
+// //     }
+// // })
+
+// // router.patch('/:postId', async (req,res) => {
+// //     try{ 
+// //         const updatePost = await Post.updateOne(
+// //             { _id: req.params.postId },
+// //             { $set: {title: req.body.title }})
+
+// //         res.json(updatePost)
         
-//     }catch(err){
-//         res.json({ message: err })
-//     }
-// })
+// //     }catch(err){
+// //         res.json({ message: err })
+// //     }
+// // })
 
-module.exports = router;
+// module.exports = router;
