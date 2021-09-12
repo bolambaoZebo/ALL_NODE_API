b58c50e4d1403b17aa4865c97916c286eb28a360
bolamboaz <bolamboaz@gmail.com>
2021-09-12 15:41:33 +0800

    add crud in post route

--- routers/posts.js
+++ routers/posts.js
@@ -27,4 +27,35 @@ router.post('/', async (req,res) => {
     }
 })
 
+router.get('/:postId', async (req,res) => {
+    try{
+        const post = await Post.findById(req.params.postId)
+        res.json(post)
+    }catch (err){
+        res.json({ message: err })
+    }
+})
+
+router.delete('/:postId', async (req,res) => {
+    try{
+        const removePost = await Post.remove({_id: req.params.postId})
+        res.json(removePost)
+    }catch (err){
+        res.json({ message: err })
+    }
+})
+
+router.patch('/:postId', async (req,res) => {
+    try{ 
+        const updatePost = await Post.updateOne(
+            { _id: req.params.postId },
+            { $set: {title: req.body.title }})
+
+        res.json(updatePost)
+        
+    }catch(err){
+        res.json({ message: err })
+    }
+})
+
 module.exports = router;
