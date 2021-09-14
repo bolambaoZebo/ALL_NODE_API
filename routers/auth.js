5484e4d2ef7beb9385918555e56e3612c017603e
bolamboaz <bolamboaz@gmail.com>
2021-09-14 11:32:54 +0800

    debug

--- routers/auth.js
+++ routers/auth.js
@@ -54,7 +54,7 @@ router.post('/register', async (req,res) => {
 router.post('/login', async (req,res) => {
 
     console.log('login path')
-    console.log(req.body.phone,req.body.password)
+    console.log(req.body)
 
     const { error } = loginValidation(req.body)
     if (error) return res.status(400).send(error.details[0].message)
